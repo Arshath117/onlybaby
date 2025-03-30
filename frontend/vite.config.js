92c543bd5fa3ffe0c9c84baf2b3535a287ad7245
Arshath117 <arshath170803@gmail.com>
2025-03-30 12:06:14 +0530

    updation

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,8 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  optimizeDeps: {
-        exclude: ["razorpay"],
-    },
   plugins: [react()],
+  build: {
+    outDir: "dist", 
+  },
+  optimizeDeps: {
+    exclude: ["razorpay"],
+  }
 })
