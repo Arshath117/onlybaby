c4cd6f180c1b68f6072b5585b9551240cd815a2b
Arshath117 <arshath170803@gmail.com>
2025-03-30 15:38:43 +0530

    updation

--- backend/server.js
+++ backend/server.js
@@ -30,13 +30,13 @@ const corsOptions = {
 
 app.use(cors(corsOptions));  
 
-if(process.env.NODE_ENV === "production"){
-  console.log("production mode")
-  app.use(express.static(path.join(__dirname,"/frontend/dist"))); 
-  app.get("*",(req,res)=>{
-      res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"));
-  });
-}
+// if(process.env.NODE_ENV === "production"){
+//   console.log("production mode")
+//   app.use(express.static(path.join(__dirname,"/frontend/dist"))); 
+//   app.get("*",(req,res)=>{
+//       res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"));
+//   });
+// }
 
 app.use("/api/auth/", authRoutes);
 app.use("/api/", productRoutes);
